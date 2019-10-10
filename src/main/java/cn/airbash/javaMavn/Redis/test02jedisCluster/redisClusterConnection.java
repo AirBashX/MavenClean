50f903ec228d3f7d43afa14c0a15b1691b5c180b
ZSP <294379349@qq.com>
2019-10-10 08:31:42 +0800

    例行更新

--- src/main/java/cn/airbash/javaMavn/Redis/test02jedisCluster/redisClusterConnection.java
+++ src/main/java/cn/airbash/javaMavn/Redis/test02jedisCluster/redisClusterConnection.java
@@ -4,10 +4,13 @@ import java.util.HashSet;
 import java.util.Set;
 
 import redis.clients.jedis.HostAndPort;
+import redis.clients.jedis.JedisCluster;
+import redis.clients.jedis.JedisPoolConfig;
 
-public class redisClusterConnection {
-
-	private void mian() {
-		Set<HostAndPort> hashSet = new HashSet<HostAndPort>();
+public class redisClusterConnection{
+	public static void main(String[] args) {
+		JedisPoolConfig jedisPoolConfig = new JedisPoolConfig();
+		Set<HostAndPort> nodes = new HashSet<HostAndPort>();
 	}
+		
 }
