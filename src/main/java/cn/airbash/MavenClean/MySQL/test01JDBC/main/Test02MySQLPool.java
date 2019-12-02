5f8d670e40188efea68d0bc9274c816b61bedc3c
ZSP <294379349@qq.com>
2019-12-03 06:43:30 +0800

    例行上传

--- src/main/java/cn/airbash/MavenClean/MySQL/test01JDBC/main/Test02MySQLPool.java
+++ src/main/java/cn/airbash/MavenClean/MySQL/test01JDBC/main/Test02MySQLPool.java
@@ -3,26 +3,18 @@ package cn.airbash.MavenClean.MySQL.test01JDBC.main;
 import java.sql.Connection;
 import java.sql.SQLException;
 
-import org.apache.commons.dbcp.BasicDataSource;
-
+import org.apache.commons.dbcp2.BasicDataSource;
 
 public class Test02MySQLPool {
-	public static void main(String[] args) {
-		//1.创建连接池对象
+	public static void main(String[] args) throws SQLException {
 		BasicDataSource dataSource = new BasicDataSource();
-		dataSource.setDriverClassName("com.mysql.jdbc.driver");
+		dataSource.setDriverClassName("com.mysql.jdbc.Driver");
 		dataSource.setUrl("jdbc:mysql://localhost:3306/test");
-		dataSource.setInitialSize(3);//初始连接数量
-		dataSource.setMaxActive(5);;//最大连接数量
 		dataSource.setUsername("root");
 		dataSource.setPassword("root");
-			Connection connection;
-			try {
-				connection = dataSource.getConnection();
-				System.out.println(connection);
-				connection.close();
-			} catch (SQLException e) {
-				e.printStackTrace();
-			}
+		dataSource.setInitialSize(3);//初始连接数量
+		dataSource.setMaxTotal(5);
+		Connection connection = dataSource.getConnection();
+		System.out.println(connection);
 	}
 }
